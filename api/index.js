465c5933b608c4179fda1df0f38cfe67bfa327bb
sumu9897 <sumon.bncd@gmail.com>
2024-12-03 12:47:31 +0600

    create sign in api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
 
@@ -17,6 +18,7 @@ mongoose.connect(process.env.MONGO).then(() =>{
 })
 
 app.use(express.json());
+app.use(cookieParser());
 const port = process.env.PORT || 3000;
 
 app.use("/api/user",userRouter);
